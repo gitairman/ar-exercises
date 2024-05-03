7c0c743ee8723e985ed85c1795e8d38c1d40ca0d
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-05-02 22:48:35 -0700

    implemented destroy callback

--- lib/employee.rb
+++ lib/employee.rb
@@ -3,4 +3,20 @@ class Employee < ActiveRecord::Base
   validates :first_name, :last_name, :store_id, presence: true
   validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
   validates :hourly_rate, numericality: { only_integer: true }
+
+  before_save :add_password
+
+  private
+
+  def rand_string(n)
+    charset = Array('A'..'Z') + Array('a'..'z')
+    Array.new(n) { charset.sample }.join
+  end
+
+  def add_password
+    self.password = rand_string(8)
+    # self.update(password: self.password)
+  end
+
+
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,13 +1,21 @@
 class Store < ActiveRecord::Base
+  before_destroy :check
   has_many :employees
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validate :must_carry_mens_or_womens_apparel
 
+
   def must_carry_mens_or_womens_apparel
     if !mens_apparel && !womens_apparel
       errors.add(:mens_apparel, "cannot be false if womens apparel is false")
       errors.add(:womens_apparel, "cannot be false if mens apparel is false")
+    end
+  end
+
+  def check
+    if self.employees.count > 1
+      throw :abort
     end
   end
 end
